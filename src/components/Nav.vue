8329f66a8b8430eac6b3d6479c44a1e9d9f96894
blanche <1935158902@qq.com>
2022-07-22 17:22:33 +0800

    创建底部路由，添加 404 页面

---
+++ src/components/Nav.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+    $END$
+    </div>
+</template>
+
+<script lang="ts">
+export default {
+name: "Nav"
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/NotFound.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+    $END$
+    </div>
+</template>
+
+<script lang="ts">
+export default {
+name: "NotFound"
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
