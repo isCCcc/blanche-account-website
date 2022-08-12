51d776dd7650d89335b5351e2afbda53cfb26309
blanche <1935158902@qq.com>
2022-08-12 21:15:50 +0800

    基本完成 Notes 组件的代码编程

--- src/components/Money/Notes.vue
+++ src/components/Money/Notes.vue
@@ -1,14 +1,18 @@
 <template>
   <label class="notes">
     <span class="name">备注</span>
-    <input type="text" placeholder="请于此处输入备注">
+    <input type="text" v-model="value" placeholder="请于此处输入备注">
   </label>
 </template>
 
 <script lang="ts">
-export default {
-  name: 'Notes'
-};
+import Vue from 'vue';
+import {Component} from 'vue-property-decorator';
+
+@Component
+export default class Notes extends Vue {
+  value = '';
+}
 </script>
 
 <style lang="scss" scoped>
