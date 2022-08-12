e39c6d96b41de35bc83e8a572a7fa0b250c71d99
blanche <1935158902@qq.com>
2022-08-12 21:03:44 +0800

    基本完成 NumberPad 组件的代码编程

--- src/components/Money/NumberPad.vue
+++ src/components/Money/NumberPad.vue
@@ -1,29 +1,78 @@
 <template>
   <div class="numberPad">
-    <div class="output">100</div>
+    <div class="output">{{ output }}</div>
     <div class="buttons">
-      <button>1</button>
-      <button>2</button>
-      <button>3</button>
-      <button>删除</button>
-      <button>4</button>
-      <button>5</button>
-      <button>6</button>
-      <button>清空</button>
-      <button>7</button>
-      <button>8</button>
-      <button>9</button>
-      <button class="ok">OK</button>
-      <button class="zero">0</button>
-      <button>.</button>
+      <button @click="outputContent">1</button>
+      <button @click="outputContent">2</button>
+      <button @click="outputContent">3</button>
+      <button @click="remove">删除</button>
+      <button @click="outputContent">4</button>
+      <button @click="outputContent">5</button>
+      <button @click="outputContent">6</button>
+      <button @click="clear">清空</button>
+      <button @click="outputContent">7</button>
+      <button @click="outputContent">8</button>
+      <button @click="outputContent">9</button>
+      <button @click="ok" class="ok">OK</button>
+      <button @click="outputContent" class="zero">0</button>
+      <button @click="outputContent">.</button>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-export default {
-  name: 'NumberPad'
-};
+import Vue from 'vue';
+import {Component} from 'vue-property-decorator';
+
+@Component
+export default class NumberPad extends Vue {
+  output = '0';
+  odd = 0;
+
+  outputContent(event: MouseEvent) {
+    const num = (event.target as HTMLButtonElement).textContent!;
+    // 限制输入位数
+    if (this.output.length >= 16) {
+      return;
+    }
+    // 排除00这种情况
+    if (this.output === '0') {
+      if ('0123456789'.indexOf(num) >= 0) {
+        this.output = num;
+      } else {
+        this.output += num;
+      }
+      return;
+    }
+    if (this.output.indexOf('.') >= 0) {
+      this.odd = this.output.split('.')[1].length + 1;
+      // 实现只能输入一个小数点的功能
+      if (num === '.') {
+        return;
+      }
+      // 实现小数点后只能输入两位数字
+      if (this.odd > 2) {
+        return;
+      }
+    }
+    this.output += num;
+  }
+
+  remove() {
+    if (this.output.length == 1) {
+      this.output = '0';
+    } else
+      this.output = this.output.slice(0, -1);
+  }
+
+  clear() {
+    this.output = '0';
+  }
+
+  ok() {
+    console.log('okok');
+  }
+}
 </script>
 
 <style lang="scss" scoped>
@@ -36,6 +85,7 @@ export default {
     font-family: Consolas, monospace;
     padding: 9px 16px;
     text-align: right;
+    height: 72px;
   }
 
   .buttons {
