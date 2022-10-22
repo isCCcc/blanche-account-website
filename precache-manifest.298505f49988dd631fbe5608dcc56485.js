3d249802e0e469f78bf522b1a2e83eb88da33fff
blanche <1935158902@qq.com>
2022-10-22 13:44:16 +0800

    update

---
+++ precache-manifest.298505f49988dd631fbe5608dcc56485.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d90cf5b42f408b91f62c",
+    "url": "/blanche-account-website/css/app.2e4429b4.css"
+  },
+  {
+    "revision": "4b68708ac4f15be3811598b80cd701f1",
+    "url": "/blanche-account-website/index.html"
+  },
+  {
+    "revision": "d90cf5b42f408b91f62c",
+    "url": "/blanche-account-website/js/app.a79cbaf1.js"
+  },
+  {
+    "revision": "89d2537cc9d42a4d369a",
+    "url": "/blanche-account-website/js/chunk-vendors.6c42e10c.js"
+  },
+  {
+    "revision": "e6af8d6ddbe409dba4a459055e703795",
+    "url": "/blanche-account-website/manifest.json"
+  },
+  {
+    "revision": "152521a9aa7494ba38786fa71e06d8ce",
+    "url": "/blanche-account-website/qrcode.png"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/blanche-account-website/robots.txt"
+  }
+]);
